99606d8070b24a0b4d96a9dc44d093b8d65f3ded
J1min <aoiharu41@gmail.com>
2023-06-26 08:47:14 +0900

    안쓰는 util 제거

--- utils/input.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeEvent } from 'react';
-
-export const handleNumberChange = (maxLength: number, event: ChangeEvent<HTMLInputElement>) => {
-  const inputValue = event.target.value;
-  if (maxLength && inputValue.length > maxLength) {
-    event.target.value = inputValue.slice(0, maxLength);
-  }
-};
